b307aef006be8279fe416df358b34d4e125d3362
lindseydrumm <ldrumm615@gmail.com>
2026-01-12 14:10:36 -0500

    adding xcode project and files

---
+++ SwimApp/SwimAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwimAppApp.swift
+//  SwimApp
+//
+//  Created by Lindsey Drumm on 1/11/26.
+//
+
+import SwiftUI
+
+@main
+struct SwimAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
